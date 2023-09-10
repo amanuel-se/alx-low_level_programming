4daf81d245384fc13c503ea10a15243da687e996
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-09 22:50:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the entry point of the program. It is where the program starts executing.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

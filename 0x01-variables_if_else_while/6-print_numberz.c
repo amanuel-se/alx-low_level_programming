894b639087de8526be46ac169cd1aa6248cace77
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-11 16:50:34 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+        for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}

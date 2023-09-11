af86005bcaa4f75554d0bfc6861bd35039efc892
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-11 15:38:55 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch = e) || (ch = q)
+		continue;
+	else
+		putchar(ch);
+        }
+	putchar('\n');
+	return (0);}

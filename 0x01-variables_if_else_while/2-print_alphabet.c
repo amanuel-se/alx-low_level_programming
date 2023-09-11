a88b8e63a07fbff07025ddbe4433c4b607ffa481
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-11 12:06:52 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a', ch <= 'z', ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}

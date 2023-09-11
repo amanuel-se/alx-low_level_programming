3b69b67f7bf7bfcf0d6b176e198ecee424fd0758
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-11 15:23:40 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,15 +11,15 @@
 int main(void)
 {
         char ch;
-
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                putchar(ch);
-        }
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	{	
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
 	}
-        putchar('\n');
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
         return (0);
 }

759fe5945a5778d1c9cd0a1a8a55425ba19c020b
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-13 12:33:47 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,36 +3,26 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
- * main - Entry point of the program
+ * print_alphabet - Entry point of the program
  *
  * Description: This is the entry point of the program.
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
-
-int main(void)
-{
-	void print_alphabet(void);
-	void print_alphabet(void);
-	void print_alphabet(void);
-	void print_alphabet(void);
-	void print_alphabet(void);
-	void print_alphabet(void);
-	void print_alphabet(void);
-	void print_alphabet(void);
-	void print_alphabet(void);
-	void print_alphabet(void);
-	return (0);
-}
-
-
 void print_alphabet(void)
 {
-        char letter;
+        char ch;
+	int i = 0;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
+	while(i<10)
+	{
+		ch = 'a'
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }

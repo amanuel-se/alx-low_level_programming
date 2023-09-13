2a9280b8a04628b6ea5c4d25b1c2441fa6c90081
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-13 11:00:06 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+
+- main - Entry point of the program
+- 
+- Description: This is the entry point of the program.
+- 
+- Return: Always 0 (Success)
+*/
+int main(void)
+{
+	putchar(\n);
+	return (0);
+}

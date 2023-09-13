50b047d5ac272547eecf485feb48d43c0e7da0e4
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-13 15:28:26 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,7 +6,8 @@
  * add - Entry point of the program
  *
  * Description: This is the entry point of the program.
- *
+ * @a: int to be added to b
+ * @b: int to be added to a
  * Return: addition
  */
 int add(int x, int y)

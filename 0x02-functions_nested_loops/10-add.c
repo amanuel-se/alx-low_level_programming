0702575c3464d48d42780cdaecc2a2e2db03108c
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-13 15:25:37 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * add - Entry point of the program
+ *
+ * Description: This is the entry point of the program.
+ *
+ * Return: addition
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}

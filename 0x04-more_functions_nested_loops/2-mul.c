99c557f8eab21b72215662f2f6610664ad6d3e44
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-16 02:47:23 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,9 +6,10 @@
  * mul - Entry point of the program
  *
  * Description: This is the entry point of the program.
+ *
  * @a: first int
  * @b: second int 
- * Return: multiplicaion
+ * Return: multiplication
  */
 int mul(int a, int b)
 {

25ba6887ad99144f79ed5c592d983990f7f421e0
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-16 02:26:01 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * mul - Entry point of the program
+ *
+ * Description: This is the entry point of the program.
+ * @a : first int
+ * @b : second int
+ * Return : multiplicaion
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

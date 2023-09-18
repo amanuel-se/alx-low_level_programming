5836b2c42e63b84da645073c5343c615ca0ff93c
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-18 13:19:48 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * reset_to_98 - Entry point of the program
+ *
+ * Description: This is the entry point of the program.
+ * @n : to check
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

67568fba514e1bd65f06ed0795f1e246dc9d319a
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-18 14:09:05 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * _strlen - Entry point of the program
+ *
+ * Description: This is the entry point of the program.
+ * @s: to check
+ * Return: 0
+ *
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		*s++;
+	}
+	return (length);
+}

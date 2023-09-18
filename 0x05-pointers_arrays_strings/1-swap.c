c088b06d5276c476e8da3a82010d3d5b7c19791d
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-18 13:37:25 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * swap_int - Entry point of the program
+ *
+ * Description: This is the entry point of the program.
+ * @a: to check
+ * @b: to check
+ * Return: 0
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}

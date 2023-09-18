44a38cf04a6b39013c13630c2201a9e5df28efdd
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-18 14:20:12 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * _puts - Entry point of the program
+ *
+ * Description: This is the entry point of the program.
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}

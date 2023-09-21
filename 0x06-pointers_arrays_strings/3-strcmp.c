2a831eb9fc5b216a59de681c41381286b623551b
Amanuel Abraham Kahssay <amanalba@gmail.com>
2023-09-21 10:28:52 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[0] != '\0' && s2[0] != '\0')
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-			i++;
-		}
-		return (0);
+		i = s1[0] - s2[0];
+		return (i);
 	}
-}
+}	
